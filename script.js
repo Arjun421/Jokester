7fc0b3454da52acefa17485be979c425e07ce8d4
Arjun Singh <arjunsingh@Arjuns-MacBook-Pro.local>
2025-02-19 21:31:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.148.0";
+import openSimplexNoise from 'https://cdn.skypack.dev/open-simplex-noise';
+
+//VARIABLES
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const white = 0xffffff;
+const black = 0x000000;
+const yellow = 0xffc800;
+const red = 0xff0000;
+const purple = 0xa200ff;
+const green = 0x03fc2c;
+const blue = 0x031cfc;
+const cyan = 0x03d3fc;
+const pink = 0xff7dcd;
+const lightYellow = 0xffe97d;
+
+//SCENE
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(black);
+scene.fog = new THREE.Fog(black, 4, 6);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(gridHelper)
+
+//LIGHT SCENE1
+let lightTopColor = new THREE.Color(yellow);
+let lightBackColor = new THREE.Color(red);
+let rectLightColor = new THREE.Color(purple);
+
+const lightTop = new THREE.PointLight(lightTopColor, 10);
+lightTop.position.set(5, 5, 2);
+lightTop.castShadow = true;
+lightTop.shadow.mapSize.width = lightTop.shadow.mapSize.height = 10000;
+lightTop.penumbra = 0.5;
+
+const lightBack = new THREE.SpotLight(lightBackColor, 2);
+lightBack.position.set(0, -3, -1);
+
+const rectLight = new THREE.RectAreaLight(rectLightColor, 20, 2, 2);
+rectLight.position.set(1, 1, 1);
+rectLight.lookAt(0, 0, 0);
+
+scene.add(lightTop, lightBack, rectLight);
+
+//LIGHT SCENE2
